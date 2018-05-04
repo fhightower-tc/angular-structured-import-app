3f8c02fb96cb8297a021d75da0499e1a5c49c4ba
Floyd Hightower <floyd.hightower27@gmail.com>
2018-05-04 08:00:46 -0400

    Adding find and replace
    Fixes #1

--- src/app/components/import/import.component.ts
+++ src/app/components/import/import.component.ts
@@ -3,6 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { MainComponent } from '../../main.component';
 import { TransferService } from '../../services/transfer.service';
 
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
 @Component({
     selector: 'import',
     templateUrl: './import.component.html',
@@ -11,6 +19,8 @@ import { TransferService } from '../../services/transfer.service';
 export class ImportComponent implements OnInit {
     delimiter: string = '';
     rawInput: string = '';
+    findValue: string = '';
+    replaceValue: string = '';
 
     constructor(
         private main: MainComponent,
@@ -18,6 +28,18 @@ export class ImportComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+    }
+
+    findAndReplace() {
+        /* Find all instances of the "findValue" and replace it with the "replaceValue". */
+        if (this.findValue === "") {
+            // TODO: add a message here...
+            console.error("No find value given");
+            return;
+        }
+
+        this.rawInput = replaceAll(this.rawInput, this.findValue, this.replaceValue);
+        // TODO: add a message that the replacement has worked
     }
 
     submit() {
